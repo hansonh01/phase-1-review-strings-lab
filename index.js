1557b332c739cfc7c71ad3d52b40197a68f34608
Hanson <hanson.hua903@gmail.com>
2023-05-16 15:35:11 -0500

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Hanson Hua';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.charAt(0)} !`;
